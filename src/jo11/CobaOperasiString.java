841457763c5486e05414596926072247b88348ee
ASUS <ASUS@LAPTOP-53HUPL4C>
2018-08-14 12:17:05 +0700

    initial commit

--- src/jo11/CobaOperasiString.java
+++ src/jo11/CobaOperasiString.java
@@ -5,6 +5,8 @@
  */
 package jo11;
 
+import java.util.Locale;
+
 /**
  *
  * @author ASUS
@@ -12,7 +14,7 @@ package jo11;
 public class CobaOperasiString {
     public static void main(String[] args) {
         
-        String identitas = "nama lengkap / kelas / no";
+        String identitas = "Rahmawati asy syifa putri / X RPL 3 / 32";
         System.out.println("Identitas : "+ identitas);
         
         String x = "Operasi";
@@ -39,8 +41,57 @@ public class CobaOperasiString {
         System.out.println("isi x sama dengan r (Not Case Sensitive) : "
                 + x.equalsIgnoreCase(r));
         
+        System.out.println("perbandingan isi x dengan y : " + x.compareTo(y));
+        System.out.println("perbandingan isi x dengan z (Case Sensitive) : "
+                + x.compareTo(z));
+        System.out.println("perbandingan isi x dengan r (Case Sensitive) : "
+                + x.compareTo(r));
+        System.out.println("perbandingan isi x dengan r (Not Case Sensitive) : "
+                + x.compareToIgnoreCase(r));
         
+        String s = "operasi";
+        System.out.println("isi variable s : "+ s);
+        System.out.println("perbandingan isi r dengan s (Case Sensitive) : "
+                + r.compareTo(s));
+        
+        System.out.println("pada x terdapat huruf 'a' : " + x.contains("a"));
+        
+        System.out.println("isi variable x besar semua : "+ x.toUpperCase());
+        System.out.println("isi variable x kecil semua : "+ x.toLowerCase());
+        
+        String t = "  operasi string  ";
+        System.out.println("isi variable t : \""+ t +"\"");
+        System.out.println("isi variable t tanpa spasi : \""+ t.trim() +"\"");
+        
+        System.out.println("gabungkan isi variable x dan r : "+ x.concat(r));
+        
+        System.out.println("karakter mulai posisi 3 di variable x : "
+                + x.substring(3));
+        
+        System.out.println("karakter dari posisi 3-5 di variable x : "
+                + x.substring(3,6));
+        
+        String tclean = t.trim();
+        System.out.println("isi variable tclean : "+ t.trim());
+        System.out.println("rubah 'i' jadi 'E' di variable tclean : "
+                + tclean.replace("i","E"));
+        
+        String[] arrT = tclean.split(" ");
+        System.out.println("pecah kata di variable tclean : ");
+        for(int i=0;i<arrT.length;i++)
+            System.out.println(arrT[i]);
+        
+        String[] arrTr = tclean.split("r");
+        System.out.println("pecah berdasarkan huruf 'r' di variable tclean : ");
+        for(int i=0;i<arrTr.length;i++)
+            System.out.println(arrTr[i]);
+        
+        double d = 1545454000;
+        System.out.println(String.format("Uang saya %, .4f", d));
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"),
+                "Uang saya Rp %,.2f", d));
+                
                
     }
-    
+ 
 }
