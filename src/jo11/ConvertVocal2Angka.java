5dcfe24024754cf5539eca4191446549fd19d77c
ASUS <ASUS@LAPTOP-53HUPL4C>
2018-08-16 19:16:56 +0700

    Initial commit

---
+++ src/jo11/ConvertVocal2Angka.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jo11;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.Scanner;
+public class ConvertVocal2Angka {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+         
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+   
+    }
+
+
+        
+
+    
+
